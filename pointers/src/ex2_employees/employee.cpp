43e3f84aa9f2349e9d3b54f39c7d3961294d2313
Sergey <bjolyk@gmail.com>
2020-01-22 21:23:42 +0300

    Improved and explained version.

--- pointers/src/ex2_employees/employee.cpp
+++ pointers/src/ex2_employees/employee.cpp
@@ -8,7 +8,8 @@
 
 #include "employee.h"
 
-Employee* EmployeeRegister::createNewEmployee(const std::string& name, int age, const std::vector<double>& wages)
+Employee* EmployeeRegister::createNewEmployee(const std::string& name, int age,
+                                              const std::vector<double>& wages)
 {
     // Employee e;
     std::map<std::string, Employee*>::iterator it = mapOfEmp.find(name);
@@ -17,7 +18,7 @@ Employee* EmployeeRegister::createNewEmployee(const std::string& name, int age, 
         return it->second;
     }
 //    reg.mapOfEmp[name] =
-    Employee* em = new Employee(); // creating in heap
+    Employee* em = new Employee(); // creating in the heap
     em->name = name;
     em->age = age;
     // em->wages = age;
@@ -26,31 +27,59 @@ Employee* EmployeeRegister::createNewEmployee(const std::string& name, int age, 
     // eventually we will delete em;
 }
 
+Employee* EmployeeRegister::findEmployee(const std::string& name)
+{
+    std::map<std::string, Employee*>::iterator it = mapOfEmp.find(n);
+    if (it == mapOfEmp.end())
+        return nullptr;
+    return it->second;
+}
+
+
 Employee** EmployeeRegister::exportEmployeesAsArray(size_t& numOfElements)
 {
     numOfElements = mapOfEmp.size();
     Employee** arr = new Employee* [numOfElements];
-    //TODO: implement this method
+
+
+
+    //TODO: finish up with this method
+
+
+
 }
 
-//
-//
-//Employee* createNewEmployee(EmployeeRegister& reg, const std::string& name, int age, const std::vector<double>& wages)
-//{
-//    // Employee e;
-//    std::map<std::string, Employee*>::iterator it = reg.mapOfEmp.find(name);
-//    if (it != reg.mapOfEmp.end())
-//    {
-//        return it->second;
-//    }
-////    reg.mapOfEmp[name] =
-//    Employee* em = new Employee(); // creating in heap
-//    em->name = name;
-//    em->age = age;
-//    // em->wages = age;
-//    reg.mapOfEmp.insert({name, em});
-//    return em;
-//    // eventually we will delete em;
-//}
-//
-//
+void EmployeeRegister::freeAll()
+{
+    // awkward
+        //for (const std::pair<std::string, Employee*>& el : mapOfEmp )
+    // better
+    for (const StrEmployeeMapEl& el : mapOfEmp )
+    {
+        delete el.second;
+    }
+    mapOfEmp.clear();
+}
+
+
+
+// It was a free method.
+// Employee* createNewEmployee(EmployeeRegister& reg, const std::string& name,
+//    int age, const std::vector<double>& wages)
+// {
+//     // Employee e;
+//     std::map<std::string, Employee*>::iterator it = reg.mapOfEmp.find(name);
+//     if (it != reg.mapOfEmp.end())
+//     {
+//         return it->second;
+//     }
+// //    reg.mapOfEmp[name] =
+//     Employee* em = new Employee(); // creating in heap
+//     em->name = name;
+//     em->age = age;
+//     // em->wages = age;
+//     reg.mapOfEmp.insert({name, em});
+//     return em;
+//     // eventually we will delete em;
+// }
+
--- pointers/src/ex2_employees/employee.h
+++ pointers/src/ex2_employees/employee.h
@@ -14,86 +14,105 @@
 #include <utility>
 #include <vector>
 
+
+// The definition of the Employee structure.
 struct Employee {
     std::string name;
     int age;
     std::vector<double> wages;
 };
 
+
+
+// an example of forward declaration (https://en.cppreference.com/w/cpp/language/class)
 struct EmployeeRegister;
 
+// The real definition of the EmployeeRegister structure.
 struct EmployeeRegister {
-    std::map<std::string, Employee*> mapOfEmp; //TODO: typedef alias
+    //==-- types definition go first --==
 
-//    Employee createNewEmployee(std::string n, int a, std::vector<double> w)
-//    {
-//        Employee newEmp;
-//        newEmp.name = n;
-//        newEmp.age = a;
-//        newEmp.wages = w;
-//        return newEmp;
-//    }
+    // BAD: using cumbersome type identifier
+        //std::map<std::string, Employee*> mapOfEmp; //TODO: typedef alias
 
-    // method does not set the instance's state so it must be marked as const
-    size_t getCount() const
-    {
-        return mapOfEmp.size();
-    }
+    // BETTER: using an alias
+    typedef std::map<std::string, Employee*> StrEmployeeMap;
+    typedef std::pair<std::string, Employee*> StrEmployeeMapEl;
 
-    void freeAll()
-    {
-        for (const std::pair<std::string, Employee*>& el : mapOfEmp )
-        {
-            delete el.second;
-        }
-        mapOfEmp.clear();
-    }
+    //==-- then we have public fields definitions (in structures only!) --==
+    StrEmployeeMap mapOfEmp;
 
+    //==-- then we have constructors and destructor --==
     ~EmployeeRegister()
     {
         freeAll();
     }
 
-    Employee* createNewEmployee(const std::string& name, int age, const std::vector<double>& wages);
+    //==-- finaly, other methods --==
 
-//    The first implementation iterates over all the objects into the internal map structure and looks for an appropriate object
-//    Employee* findEmployee(std::string n)
-//    {
-//        Employee* wantedPointer = nullptr;
-//        for(auto& item : mapOfEmp)
-//        {
-//            if (item.first == n)
-//            {
-//                wantedPointer = item.second;
-//            }
-//        }
-//        return wantedPointer;
-//    }
+    // correctly clears all the registerd employees
+    void freeAll();
 
-//    The second implementation uses map features (fast key lookup) for getting access to the Employee object.
-    //TODO: separate interface and implementation
-    Employee* findEmployee(const std::string& n)
+    // Returns the number of registered employees.
+    //
+    // 1) Method does not set the instance's state so it must be marked as const
+    // 2) The definition of this method is provided here, because it's pretty little
+    // and could be considered by the compiler as inline
+    // https://www.learncpp.com/cpp-tutorial/89-class-code-and-header-files/
+    size_t getCount() const
     {
-        std::map<std::string, Employee*>::iterator it = mapOfEmp.find(n);
-        if (it == mapOfEmp.end())
-            return nullptr;
-        return it->second;
+        return mapOfEmp.size();
     }
 
+    Employee* createNewEmployee(const std::string& name, int age,
+                                const std::vector<double>& wages);
+
+    //  The second implementation uses map features (fast key lookup) for getting
+    //  access to the Employee object.
+    // DONE: separate interface and implementation
+    Employee* findEmployee(const std::string& name);
+
+    // Exports a collection employess as an array of pointers.
     Employee** exportEmployeesAsArray(size_t& numOfElements);
-//    void exportEmployeesAsArray() {
-//        Employee* *employees = new Employee* [mapOfEmp.size()];
-//        // Employee* emp = nullptr;
-//        std::map<std::string, Employee* >::iterator it = mapOfEmp.begin();
-//        for (int i = 0; i != sizeof(mapOfEmp) && it != mapOfEmp.end(); i++) {
-//            employees[i] = it->second;
-//            it++;
-//        }
-//    }
+
+
+    //==-- dead code --==
+
+    // wrong approach
+        //    Employee createNewEmployee(std::string n, int a, std::vector<double> w)
+        //    {
+        //        Employee newEmp;
+        //        newEmp.name = n;
+        //        newEmp.age = a;
+        //        newEmp.wages = w;
+        //        return newEmp;
+        //    }
+
+
+    // BAD approach: map allows to pick up an element for log(n) instead of n
+        //    The first implementation iterates over all the objects into the
+        //    internal map structure and looks for an appropriate object
+        //    Employee* findEmployee(std::string n)
+        //    {
+        //        Employee* wantedPointer = nullptr;
+        //        for(auto& item : mapOfEmp)
+        //        {
+        //            if (item.first == n)
+        //            {
+        //                wantedPointer = item.second;
+        //            }
+        //        }
+        //        return wantedPointer;
+        //    }
 }; // struct Employee
 
+
+// just an example that we can have more than one forward declarations
 struct EmployeeRegister;
 
-////creates and adds a new Employee object and initializes it with given parameters
-//Employee* createNewEmployee(EmployeeRegister& reg, const std::string& name, int age, const std::vector<double>& wages);
+
+// creates and adds a new Employee object and initializes it with given parameters
+// Employee* createNewEmployee(EmployeeRegister& reg, const std::string& name,
+//    int age, const std::vector<double>& wages);
+
+
 #endif // EMPLOYEE_H
